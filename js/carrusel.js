d705839d536ea708618551ddd2e7e1e5f88ac92f
Cristian Donnachie <119471458+c-donnachie@users.noreply.github.com>
2023-08-09 16:23:16 -0400

    responsive home

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
-    $('.container-img').slick({
-      slidesToShow: 6,
-      slidesToScroll: 6,
-      dots: false,
-      infinite: true
-    });
+  $('.container-img').slick({
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    dots: false,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1200, 
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+
+    ]
   });
-  
+});
