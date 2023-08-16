85f4e6fe311a0d40e96693faa75c105f224a3694
Cristian Donnachie <119471458+c-donnachie@users.noreply.github.com>
2023-08-16 01:19:39 -0400

    cambios, integracion de sass

---
+++ js/script.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 6,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+    1250: {
+      slidesPerView: 6,
+    },
+  },
+})
