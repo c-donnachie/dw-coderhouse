62580afedf38367a3000bb82aeaff3e317334f06
Cristian Donnachie <119471458+c-donnachie@users.noreply.github.com>
2023-08-22 20:02:14 -0400

    mas arreglos header, sucribe

--- js/script.js
+++ js/script.js
@@ -33,15 +33,3 @@ var swiper = new Swiper(".slide-content", {
     },
   },
 })
-
-// Menu
-const menuCheckbox = document.getElementById('menu-checkbox');
-    const mobileNav = document.getElementById('mobile-nav');
-
-    menuCheckbox.addEventListener('change', () => {
-      if (menuCheckbox.checked) {
-        mobileNav.style.display = 'block';
-      } else {
-        mobileNav.style.display = 'none';
-      }
-    });
