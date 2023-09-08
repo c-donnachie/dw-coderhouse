03680c2273cbaac22e0f4ea06147e920c0b8a86d
Cristian Donnachie <119471458+c-donnachie@users.noreply.github.com>
2023-09-07 21:58:00 -0300

    readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# PROJECTO FINAL - Coderhouse
+Projecto realizado para el curos de desarrollo web en la plataforma de Coderhouse
+
+# Descripcion
+
+El sitio es un clon no pixel perfect de hbo max,
+tambien tiene algunos cambios esteticos
+
+# Tecnologías Utilizadas
+
+- HTML
+- Sass
+- Bootstrap
+- Node.js
+
+# Autor
+
+- Nombre: Cristian Donnachie
+- Contacto: ugar.cristian@gmail.com
--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,11 @@
 // Menu
-const menuCheckbox = document.getElementById('menu-checkbox');
-    const mobileNav = document.getElementById('mobile-nav');
+const menuCheckbox = document.getElementById("menu-checkbox")
+const mobileNav = document.getElementById("mobile-nav")
 
-    menuCheckbox.addEventListener('change', () => {
-      if (menuCheckbox.checked) {
-        mobileNav.style.display = 'block';
-      } else {
-        mobileNav.style.display = 'none';
-      }
-    });
+menuCheckbox.addEventListener("change", () => {
+  if (menuCheckbox.checked) {
+    mobileNav.style.display = "block"
+  } else {
+    mobileNav.style.display = "none"
+  }
+})
